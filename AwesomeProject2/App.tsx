fe5f474b33c3ba2aef152d875b51bec5dcdd0c6b
hossam sulleman <93336567+hossam1231@users.noreply.github.com>
2023-05-28 22:05:54 +0100

    push

--- AwesomeProject2/App.tsx
+++ AwesomeProject2/App.tsx
@@ -4,6 +4,8 @@ import * as React from 'react';
 import {View, Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import {BottomSheetModalProvider} from '@gorhom/bottom-sheet';
 
 function HomeScreen() {
   return (
@@ -17,11 +19,15 @@ const Stack = createNativeStackNavigator();
 
 function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <GestureHandlerRootView style={{flex: 1}}>
+      <BottomSheetModalProvider>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Home" component={HomeScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </BottomSheetModalProvider>
+    </GestureHandlerRootView>
   );
 }
 
--- AwesomeProject2/pages/HomePage.jsx
+++ AwesomeProject2/pages/HomePage.jsx
@@ -1,11 +1,124 @@
-import React from 'react'
+// In App.js in a new project
 
-const HomePage = () => {
+import * as React from 'react';
+import {Text, ScrollView} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import {MiniMap} from '../pages/MapPage';
+import RiveComponent from '../components/RiveComponent';
+import {MyListFlatlist} from '../components/FlatlistComponent';
+import {Heading, VStack, Box, Center, HStack} from 'native-base';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import ExploreFlatlist from '../components/FlatlistComponent';
+import BottomsheetModalComponent from '../components/BottomsheetModalComponent';
+import {CustomHeader, CustomSubHeader} from '../components/TextVariations';
+
+export default function HomePage() {
+  const navigation = useNavigation();
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <ScrollView bounces={false}>
+      <Box safeArea flex="1" pt="5" px="10" bg="#020002">
+        <HStack>
+          <VStack>
+            <CustomHeader text={'DAILY PRAYERS'} />
+            <CustomSubHeader text={'Isha in 3h 20m'} />
+          </VStack>
+        </HStack>
+        <Center mt="10" h="20">
+          <RiveComponent
+            resourceName={'ratings'}
+            styleProp={{width: '100%', height: 200}}
+          />
+        </Center>
+      </Box>
 
-export default HomePage
+      <Box>
+        <Box bg="#020002">
+          <MyListFlatlist />
+        </Box>
+
+        <Box flex="1" p="10" bg="#020002">
+          <HStack mb="5" h="100" justifyContent={'space-between'}>
+            <VStack space={2}>
+              <Text
+                style={{
+                  fontSize: 30,
+                  fontFamily: 'Inter-Regular',
+                  color: 'grey',
+                }}>
+                Chosen
+              </Text>
+              <Text
+                style={{
+                  fontSize: 35,
+                  fontFamily: 'Agrandir-GrandHeavy',
+                  fontStyle: 'italic',
+                  color: 'white',
+                }}>
+                For You
+              </Text>
+            </VStack>
+            <Box>
+              <BottomsheetModalComponent
+                trigger={
+                  <Text
+                    style={{
+                      fontSize: 15,
+                      fontFamily: 'Inter-Bold',
+                      color: 'grey',
+                    }}>
+                    See more
+                  </Text>
+                }
+              />
+            </Box>
+          </HStack>
+          <ExploreFlatlist />
+          <TouchableOpacity
+            onPress={() => {
+              navigation.navigate('Map');
+            }}>
+            <Box
+              overflow="hidden"
+              my="10"
+              borderWidth="1px"
+              borderColor={'grey'}
+              rounded="xl"
+              w="100%"
+              h="200">
+              <MiniMap />
+              <Box
+                px="5"
+                justifyContent="center"
+                // roundedBottom="xl"
+                position="absolute"
+                opacity="0.7"
+                bg="black"
+                borderBottomWidth="1px"
+                borderColor={'grey'}
+                bottom="0"
+                w="100%"
+                h="10">
+                <Text
+                  style={{
+                    fontSize: 15,
+                    fontFamily: 'DINOffcPro-Black',
+                    color: 'white',
+                  }}>
+                  Find places on the map
+                </Text>
+              </Box>
+            </Box>
+          </TouchableOpacity>
+          <Center pb="10">
+            <Heading sub color="white">
+              You are not subscribed to anyone
+            </Heading>
+            <Heading sub color="grey">
+              News from subscriptions will appear here
+            </Heading>
+          </Center>
+        </Box>
+      </Box>
+    </ScrollView>
+  );
+}
